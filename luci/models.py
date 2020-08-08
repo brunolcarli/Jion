92e205bbebd64c797cb7fcf57054f59a66d31e1a
brunolcarli <brunolcarli@gmail.com>
2020-08-08 14:15:51 -0400

    ADD model de quote

--- luci/models.py
+++ luci/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 
 class Emotion(models.Model):
-    reference = models.CharField(max_length=100)
+    reference = models.CharField(max_length=100, null=False, blank=False)
     pleasantness = models.FloatField(default=0)
     attention = models.FloatField(default=0)
     sensitivity = models.FloatField(default=0)
     aptitude = models.FloatField(default=0)
+
+
+class Quote(models.Model):
+    reference = models.CharField(max_length=100, null=False, blank=False)
+    quote = models.TextField(null=False, blank=False)
+    author = models.CharField(max_length=100, null=False, blank=False)
+    date = models.DateField(auto_now_add=True)
