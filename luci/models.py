619b4dac01a71e000e9df9ba4a61959a3d735746
brunolcarli <brunolcarli@gmail.com>
2020-08-08 12:53:49 -0400

    ADD emotion models

--- luci/models.py
+++ luci/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Emotion(models.Model):
+    reference = models.CharField(max_length=100)
+    pleasantness = models.FloatField(default=0)
+    attention = models.FloatField(default=0)
+    sensitivity = models.FloatField(default=0)
+    aptitude = models.FloatField(default=0)
